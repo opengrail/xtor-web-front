cf8f67675b22155f81767cd374dce1eb4cf014f2
Ray McDermott <ray.mcdermott@toyota-europe.com>
2015-10-26 23:30:19 +0100

    datomic-connection

--- src/clojure_getting_started/web.clj
+++ src/clojure_getting_started/web.clj
@@ -45,7 +45,8 @@
         simple-jdbc (first (clojure.string/split jdbc-url #"\?"))
         uri (str "datomic:sql://" (:host datomic) ":" (:port datomic) "/customer"
                  "?" simple-jdbc
-                 "&ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory")
+                 ;"&ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory"
+                 )
         db-created! (d/create-database uri)
         conn (d/connect uri)
         db (d/db conn)
