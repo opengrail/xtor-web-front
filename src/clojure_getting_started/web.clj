52fbeae2d6114d1d710247de4797743bafbc66af
Ray McDermott <ray.mcdermott@toyota-europe.com>
2015-12-22 00:48:11 +0100

    :bug: ok I did comment out starting jetty too

--- src/clojure_getting_started/web.clj
+++ src/clojure_getting_started/web.clj
@@ -97,9 +97,9 @@
            (ANY "*" []
              (route/not-found (slurp (io/resource "404.html")))))
 
-;(defn -main [& [port]]
-;  (let [port (Integer. (or port (env :port) 5000))]
-;    (jetty/run-jetty (site #'app) {:port port :join? false})))
+(defn -main [& [port]]
+  (let [port (Integer. (or port (env :port) 5000))]
+    (jetty/run-jetty (site #'app) {:port port :join? false})))
 
 ;; For interactive development:
 ;; (.stop server)
